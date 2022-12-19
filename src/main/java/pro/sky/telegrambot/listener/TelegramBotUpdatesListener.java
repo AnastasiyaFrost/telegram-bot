12f83b37e8b9c89d4a82e7bb82b26ba116155460
user <grebneva.nastena@yandex.ru>
2022-12-19 20:21:38 +0300

    Работа готова, но вопрос по инджекту репозитория, приложу скрины

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -8,17 +8,31 @@ import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import pro.sky.telegrambot.entity.NotificationTask;
+import pro.sky.telegrambot.repository.NotificationTaskRepository;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
     private static String STR_CMD = "/start";
-    private static String GREET_MSG = "Welcome to chatbot!";
+    private static String GREET_MSG = "Добро пожаловать в чатбот-напоминалку!";
+    private static String WRNG_FORM_MSG = "Команда не распознана. " +
+            "Пожалуйста, проверьте формат введенного сообщения";
+
+    private static Pattern pattern = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W+]+)");
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+
+    public TelegramBotUpdatesListener(NotificationTaskRepository notificationTaskRepository) {
+        this notificationTaskRepository = notificationTaskRepository;
+    }
 
     @Autowired
     private TelegramBot telegramBot;
@@ -34,22 +48,48 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             logger.info("Processing update: {}", update);
             Message message = update.message();
 
+
             if (message.text().equals(STR_CMD)) {
                 logger.info("Command's been recieved: " + STR_CMD);
                 sendMessage(showChatId(message), GREET_MSG);
             } else {
+                Matcher matcher = pattern.matcher(message.text());
 
+                if (matcher.matches()) {
+                    logger.info("Task's been recieved.");
+                    String date = matcher.group(1);
+                    String item = matcher.group(3);
+                    LocalDateTime dateTime = LocalDateTime.parse(date,
+                            DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
+
+                    NotificationTask task = new NotificationTask();
+                    task.setChatId(showChatId(message));
+                    task.setNotificationSendDatetime(dateTime);
+                    task.setMessageContent(item);
+                    notificationTaskRepository.save(task);
+                } else {
+                    logger.info("Wrong format. Command wasn`t recognized.");
+                    sendMessage(showChatId(message), WRNG_FORM_MSG);
+                }
             }
-            ;
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
+
+    @Scheduled(cron = "0 0/1 * * * *")
+    public void run() {
+        List<NotificationTask> send = notificationTaskRepository.getTasksToSend();
+        send.forEach(task -> {
+            logger.info("Sending a task: {}", task);
+            sendMessage(task);
+        });
+    }
+
 
     private void sendMessage(Long chatId, String messageContent) {
         SendMessage sendMessage = new SendMessage(chatId, messageContent);
         telegramBot.execute(sendMessage);
     }
-
 
     private void sendMessage(NotificationTask task) {
         sendMessage(task.getChatId(), task.getMessageContent());
--- src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
@@ -1,9 +1,16 @@
 package pro.sky.telegrambot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import pro.sky.telegrambot.entity.NotificationTask;
 
 import javax.persistence.Id;
+import java.util.List;
 
+@Repository
 public interface NotificationTaskRepository extends JpaRepository<NotificationTask, Id> {
+    @Query(value = "SELECT * FROM DB_teleBot WHERE notificationSendDatetime = " +
+            "LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES)", nativeQuery = true)
+    List<NotificationTask> getTasksToSend();
 }
